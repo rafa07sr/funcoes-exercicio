1237b2cc3e746756923e41cc0d8e8e5e8cc05cfd
rafa07sr <rafa.07.g3@mail.com>
2022-10-25 18:44:22 -0300

    exercicios feitos

---
+++ exercicio3/script.js
@@ -0,0 +1,27 @@
+//exercicio 3
+
+function soma (num1, num2){
+
+    return num1+num2
+}
+function subtracao (num1, num2){
+
+    return num1-num2
+}
+function divisao (num1, num2){
+
+    return num1/num2
+}
+function multiplicacao (num1, num2){
+
+    return num1*num2
+}
+
+
+let numero1 = 50
+let numero2 = 10
+
+console.log(soma(numero1,numero2))
+console.log(subtracao(numero1,numero2))
+console.log(divisao(numero1,numero2))
+console.log(multiplicacao(numero1,numero2))
