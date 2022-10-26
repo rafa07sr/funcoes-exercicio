f86dfb4d719a131d4cb9644e52619c301831d5d0
rafa07sr <rafa.07.g3@mail.com>
2022-10-25 21:12:24 -0300

    exercicio funções pronto

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,10 +1,10 @@
 //exercício a
 
-function meuNome(nome){
+function meuNome(nome) {
 
     return nome
 
-}    
+}
 
 console.log(`olá meu nome é`, meuNome("Rafael"))
 
@@ -12,20 +12,20 @@ console.log(`olá meu nome é`, meuNome("Rafael"))
 
 const tabuada = (num) => {
 
-    console.log ("Vamos aprender sobre a Tabuada")
+    console.log("Vamos aprender sobre a Tabuada")
 
-    console.log(`${num}x 1 = ${num*1} `)
-    console.log(`${num}x 2 = ${num*2} `)
-    console.log(`${num}x 3 = ${num*3} `)
-    console.log(`${num}x 4 = ${num*4} `)
-    console.log(`${num}x 5 = ${num*5} `)
-    console.log(`${num}x 6 = ${num*6} `)
-    console.log(`${num}x 7 = ${num*7} `)
-    console.log(`${num}x 8 = ${num*8} `)
-    console.log(`${num}x 9 = ${num*9} `)
-    console.log(`${num}x 10 = ${num*10} `)
-    
-    
+    console.log(`${num}x 1 = ${num * 1} `)
+    console.log(`${num}x 2 = ${num * 2} `)
+    console.log(`${num}x 3 = ${num * 3} `)
+    console.log(`${num}x 4 = ${num * 4} `)
+    console.log(`${num}x 5 = ${num * 5} `)
+    console.log(`${num}x 6 = ${num * 6} `)
+    console.log(`${num}x 7 = ${num * 7} `)
+    console.log(`${num}x 8 = ${num * 8} `)
+    console.log(`${num}x 9 = ${num * 9} `)
+    console.log(`${num}x 10 = ${num * 10} `)
+
+
 
 }
 
